84a8061764c98449c85a31e321fb17b8bd2e0f8e
GabeGramblicka <gabegramblicka@protonmail.com>
2025-02-17 10:02:36 -0700

    added new gamedir program

---
+++ Gamedir.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cstdlib>
+
+int main(void) {
+  std::cout << "cd ../../Drives/Data/Projects/DesolationOfATerrribleLand/" << std::endl;
+  system("cd ../../Drives/Data/Projects/DesolationOfATerrribleLand/");
+  return 0;
+}
