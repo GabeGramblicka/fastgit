5bb47de3c36067a5751f3d6af1946148c2cba51b
GabeGramblicka <gabegramblicka@protonmail.com>
2026-01-27 10:22:12 -0700

    more flags for fastgit

--- fastgit.sh
+++ fastgit.sh
@@ -1,6 +1,43 @@
 #!/bin/bash
-git add .
-read -p "Commit Message: " msg
-git commit -m "$msg"
-read -p "Push? " -n1
-git push
+if [ $# -eq 0 ]; then
+  git add .
+  read -p "Commit Message: " msg
+  git commit -m "$msg"
+  read -p "Push? " -n1
+  git push
+  exit 1
+fi
+
+function usage() {
+  cat <<USAGE
+
+  Usage: $0 [-c count]
+
+  Options:
+    -c, --count: total commit count of repository 
+
+USAGE
+  exit 1
+}
+
+for arg in "$@"; do 
+  case $arg in
+  -v | --verbose)
+    git status -s
+    echo -e -n "\nAdd all new files listed above?"
+    read
+    git add .
+    read -p "Commit Message: " msg
+    git commit -m "$msg"
+    read -p "Push? " -n1
+    git push
+    ;;
+  -c | --count)
+    echo -n "Total commit count is "; git log --pretty=format:'' | wc -l
+    ;;
+  -h | --help)
+    usage
+    ;;
+  esac
+done
+  
